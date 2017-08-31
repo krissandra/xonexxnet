91bfc08b6a5105df504f718176e39f94332abf3c
krissandra <31430349+krissandra@users.noreply.github.com>
2017-08-31 15:46:24 +0700

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -22,11 +22,11 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="/2017/08/tips-mudah-memudarkan-warna-rambut.html"}
-if(x=="2"){xxx="/2017/08/tips-merawat-rambut-untuk-pengendara.html"}
-if(x=="3"){xxx="/2017/08/jamur-tiram-baik-bagi-kesehatan.html"}
-if(x=="4"){xxx="/2017/08/cara-mengatasi-diabetes-secara-alami.html"}
-if(x=="5"){xxx="/2017/08/6-makanan-yang-tidak-boleh-dimakan.html"}a_to_ve[i].href="https://bagamanacarauntuk.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="/2017/08/bahaya-memakai-headset-bagi-kesehatan.html"}
+if(x=="2"){xxx="/2017/08/bagian-tubuh-yang-seringkali-dilupakan.html"}
+if(x=="3"){xxx="/2017/08/tips-mengatasi-mata-sembab-di-pagi-hari.html"}
+if(x=="4"){xxx="/2017/08/6-cara-terbaik-mengkontrol-berat-badan.html"}
+if(x=="5"){xxx="/2017/08/cara-mengatasi-perut-mual-secara-alami.html"}a_to_ve[i].href="http://www.cariinformasiuntuk.ga"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
